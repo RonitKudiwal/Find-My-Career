3456d4a7b0b770787bce3c5ae0b36d7aaeada7c3
Ronit Kudiwal <ronitkumawat75@gmail.com>
2024-11-18 23:31:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of a simple JavaScript function for the Career Test button
+function startTest() {
+    alert("Starting Career Test... (This would lead to a test page or form in a real implementation)");
+}
